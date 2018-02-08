f3dae2f70817c8fe29321e0e3b93b3e387525500
Shrey Agarwal <shrey.agarwal2@gmail.com>
2018-02-08 02:15:28 -0800

    Adding Project 1

---
+++ Project 1/utils.py
@@ -0,0 +1,49 @@
+import pickle
+import matplotlib.pyplot as plt
+import numpy as np
+import itertools
+
+def save(obj,filename,directory=''):
+	pickle.dump(obj,open(directory+filename+'.pkl','wb'))
+
+def load(filename,directory=''):
+	return pickle.load(open(directory+filename+'.pkl','rb'))
+
+def save_plot(img,filename,directory=''):
+    img.savefig(directory+'/'+filename + '.png')
+    
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Oranges):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    fig=plt.figure(figsize=(6, 6))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+    print (cm.shape)
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, "%.2f"%cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
+    return fig
